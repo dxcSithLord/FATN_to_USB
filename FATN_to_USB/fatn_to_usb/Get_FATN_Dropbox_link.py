c14d8e49959110a0a890ed6b83d11d739914bdd7
Alistair Amabile <48163392+dxcSithLord@users.noreply.github.com>
2020-11-30 18:55:59 +0000

    Update with known fixes

--- FATN_to_USB/fatn_to_usb/Get_FATN_Dropbox_link.py
+++ FATN_to_USB/fatn_to_usb/Get_FATN_Dropbox_link.py
@@ -1,12 +1,14 @@
+#!/usr/bin/env python3
 #/* FATN_Parser.pl */
 
 from html.parser import HTMLParser
-#from html.entities import name2codepoint
-#from lxml import html
 import urllib
 
 class GetFATNUrl(HTMLParser):
-    
+    ''' Extend HTMLParser template to handle FATN Website, looking for
+    dropbox tinyurl link.  Define handle_starttag to checkfor 
+    specific condition and extract link into self.FATNurl
+    '''
     def handle_starttag(self, tag, attrs):
         if tag == 'a':
             for name, value in attrs:
@@ -22,7 +24,7 @@ def Download_FATN(url: str, folder_name:str):
     if not os.path.exists(folder_name):
         os.makedirs(folder_name) # create folder if it does not exist
         
-    local_filename = 'FATN News Weeekly File.zip'
+    local_filename = 'FATN News Weekly File.zip'
     path=os.path.join(folder_name, local_filename)
     r = requests.get(url, stream=True)
     if r.ok:
@@ -43,12 +45,14 @@ FATNparse = GetFATNUrl()
 
 FATN = urllib.request.urlopen(url)  # get the FATN wordpress webpage
 FATNparse.feed(str(FATN.read()))    # parse it for the dropbox link
-print(FATNparse.FATNurl)
+# print(FATNparse.FATNurl)
 
-FATN2 = urllib.request.urlopen(FATNparse.FATNurl) # Get dropbox url from link
-print("FATN Dropbox URL:", FATN2.geturl())
-FATNdl= str(FATN2.geturl()).replace('?dl=0','?dl=1') # set the download bit
-print( FATNdl )
-Download_FATN(FATNdl, '/home/pi/' ) # go and get the file
+FATNdropboxurl = urllib.request.urlopen(FATNparse.FATNurl) # Get dropbox url from link
+print("FATN Dropbox URL:", FATNdropboxurl.geturl())
+
+FATNdownload= str(FATNdropboxurl.geturl()).replace('?dl=0','?dl=1') # set the download bit
+print( FATNdownload )
+
+Download_FATN(FATNdownload, '/home/pi/' ) # go and get the file
 
 
